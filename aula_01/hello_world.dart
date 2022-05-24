36656adac667abafaf6ae9e32c6a9e244f176bb7
Denilsom <denilsomlobo@gmail.com>
2022-05-24 20:49:15 -0300

    first commit

---
+++ aula_01/hello_world.dart
@@ -0,0 +1,9 @@
+//Curso de Tencnologia da Informação
+//Prof Sebastião Marcos
+//Primeiro programa em Dart: Hello World!
+
+void main() {
+  //Primeiro Programa em Dart
+  //Printando no terminal com Code Runner
+  print('Hello World!!!');
+}
