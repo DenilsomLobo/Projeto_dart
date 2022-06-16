8154d64c9bb1debe68cdd888fb1f397b8a2e1180
Denilsom <denilsomlobo@gmail.com>
2022-06-15 21:30:05 -0300

    15/06/2022

--- atividade_dart_02/letra_f.dart
+++ atividade_dart_02/letra_f.dart
@@ -2,3 +2,36 @@
 //Atividade Dart 02: Letra F
 //Aluno: Denilsom Lobo de Souza
 //Turma: 11
+
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  stdout.write('Digite um número entre 0 e 100: ');
+  int numero = int.parse(stdin.readLineSync()!);
+  while(true) {
+  print(adivinharNumero(numero));
+  }
+}
+
+dynamic adivinharNumero(int numero) {
+    int escolhaComputadorNumero = Random().nextInt(101);
+    print(escolhaComputadorNumero);
+
+    if (escolhaComputadorNumero > numero) {
+      int resultado = escolhaComputadorNumero - numero;
+      if (resultado <= 10) {
+        return 'Está perto';
+      } else {
+        return 'Muito alto';
+      }
+    } else if (numero > escolhaComputadorNumero) {
+      int resultado = numero - escolhaComputadorNumero;
+      if (resultado <= 10) {
+        return 'Está perto';
+      } else {
+        return 'Muito alto';
+      }
+    }
+  }
+
--- atividade_dart_02/letra_j.dart
+++ atividade_dart_02/letra_j.dart
@@ -2,3 +2,34 @@
 //Atividade Dart 02: Letra J
 //Aluno: Denilsom Lobo de Souza
 //Turma: 11
+
+import 'dart:io';
+
+void main() {
+
+  stdout.write('Insira o primeiro valor: ');
+  double valor1 = double.parse(stdin.readLineSync()!);
+
+  stdout.write('Insira o segundo valor: ');
+  double valor2 = double.parse(stdin.readLineSync()!);
+
+  stdout.write('Insira o terceiro valor: ');
+  double valor3 = double.parse(stdin.readLineSync()!);
+
+  List lista = funcaoDobro(valor1, valor2, valor3);
+  print(lista);
+
+  for(var i in lista) {
+    print('Valor + 10 = ${i + 10}');
+  }
+}
+
+List<double> funcaoDobro(double a, double b, double c) {
+  List<double> listaDobro = [];
+
+  listaDobro.add(a * 3);
+  listaDobro.add(b * 3);
+  listaDobro.add(c * 3);
+  return listaDobro;
+}
+
