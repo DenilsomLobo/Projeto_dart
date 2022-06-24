275412b594075f82423b4b36b9309c715051666c
Denilsom <denilsomlobo@gmail.com>
2022-06-23 21:37:46 -0300

    23/06/2022

---
+++ atividade_dart_03/letra_a/main.dart
@@ -0,0 +1,12 @@
+//Curso tecnico de informatica Senac
+//Atividade Dart 03: Letra A
+//Aluno: Denilsom Lobo de Souza
+//Turma: 11
+
+
+void main() {
+  print('-'*70);
+  print('CÁLCULO TRAPÉZIO');
+  print('-'*70);
+  
+}
