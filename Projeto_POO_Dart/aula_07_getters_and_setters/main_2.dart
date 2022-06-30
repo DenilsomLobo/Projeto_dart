31646d94e53edeb82f40fb1a9aacaf007a8eaedd
Denilsom <denilsomlobo@gmail.com>
2022-06-29 21:39:49 -0300

    29/06/2022

---
+++ Projeto_POO_Dart/aula_07_getters_and_setters/main_2.dart
@@ -0,0 +1,14 @@
+import 'model/pessoa.dart';
+
+void main() {
+  Pessoa paciente = new Pessoa();
+  paciente.nome = 'John Doe';
+  paciente.setCpf = '123.456.789-12';
+  paciente.setAltura = 1.2;
+  paciente.peso = 80.0;
+
+  print('Nome do paciente: ${paciente.nome}');
+  print('CPF do paciente: ${paciente.getCpf}');
+  print('Altura do paciente: ${paciente.getAltura}');
+  print('Peso do paciente: ${paciente.peso}');
+}
